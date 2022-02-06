289911cfc3f015eae760717093576e068673165a
E-Molostova <molostova8973@gmail.com>
2022-02-06 12:50:52 +0200

    first part of hw5

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ require("dotenv").config();
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth");
 const userRouter = require("./routes/api/users");
-const { User } = require("./model");
 
 const app = express();
 
@@ -15,6 +14,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
